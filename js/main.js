5eec43e443f9318fff4e34d84c27da5c3431aa59
BrunaPires <b.serip@gmail.com>
2020-12-16 12:05:50 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,63 @@
+/*---------------Roteiro JavaScript-----------------*/
+
+//alert
+//alert("meu primeiro js");
+
+
+//----------------------------------------------//
+//variaveis
+//exemplo 1
+// var nome = "Bruna Pires";
+// alert("Bem vindo " + nome);
+
+//exemplo 2
+// var nome = "Bruna Pires";
+// var idade = 32;
+// alert(nome + " tem " + idade + " anos");
+
+//exemplo 3 - soma
+// var nome = "Bruna Pires";
+// var idade1 = 32;
+// var idade2 = 10;
+// alert(nome + " tem " + (idade1 + idade2) + " anos");
+
+//----------------------------------------------//
+//console
+// var nome = "Bruna Pires";
+// var idade1 = 32;
+// var idade2 = 10;
+// console.log(nome);
+// console.log(idade1 + idade2);
+
+
+//----------------------------------------------//
+//replace
+// var nome = "Bruna Pires";
+// var idade1 = 32;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// console.log(nome);
+// console.log(idade1 + idade2);
+// console.log(frase.replace("Japão", "Brasil"));
+// alert(frase.replace("Japão", "Brasil"));
+
+//----------------------------------------------//
+//toUpperCase
+// var nome = "Bruna Pires";
+// var idade1 = 32;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// console.log(nome);
+// console.log(idade1 + idade2);
+// console.log(frase.toUpperCase ("Japão", "Brasil"));
+
+
+//----------------------------------------------//
+//toLowerCase
+// var nome = "Bruna Pires";
+// var idade1 = 32;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// console.log(nome);
+// console.log(idade1 + idade2);
+// console.log(frase.toLowerCase ("Japão", "Brasil"));
